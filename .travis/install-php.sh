a32faba5f38fd516e966635d64c14f8252408e55
Herman J. Radtke III <herman@hermanradtke.com>
2017-10-14 09:27:01 -0700

    Build against 7.1.10

--- .travis/install-php.sh
+++ .travis/install-php.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-if [ ! -f "$HOME/php-src/libs/libphp7.so" ]; then
+VERSION=$1
+
+if [ ! -f "$HOME/$VERSION/libs/libphp7.so" ]; then
   cd $HOME
-  rm -fr php-src
-  curl -o php-7.1.8.tar.gz http://us1.php.net/distributions/php-7.1.8.tar.gz
-  tar xzf php-7.1.8.tar.gz
-  mv php-7.1.8 php-src
-  cd php-src
+  rm -fr $VERSION
+  curl -o $VERSION.tar.gz http://php.net/distributions/$VERSION.tar.gz
+  tar xzf $VERSION.tar.gz
+  cd $VERSION
   sed -e 's/void zend_signal_startup/ZEND_API void zend_signal_startup/g' -ibk Zend/zend_signal.c Zend/zend_signal.h
   ./configure --enable-debug --enable-embed=shared --enable-maintainer-zts
   make
